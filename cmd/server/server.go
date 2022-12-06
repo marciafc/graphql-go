2b0ce9ac34fb83cea93b42c8ed93ecd5c8beb0ec
Marcia Castagna <marciafc.info@gmail.com>
2022-12-06 00:35:21 -0300

    Aula 7 - Persistindo Category via Playground

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
@@ -8,17 +9,32 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/marciafc/graphql-go/graph"
+	"github.com/marciafc/graphql-go/internal/database"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const defaultPort = "8080"
 
 func main() {
+
+	// Abrir conexão com o banco de dados
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("failed to open database: %v", err)
+	}
+	defer db.Close()
+
+	// categoryDb -> para inserir com NewCategory
+	categoryDb := database.NewCategory(db)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{		
+		CategoryDB: categoryDb,
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
--- graph/resolver.go
+++ graph/resolver.go
@@ -1,5 +1,6 @@
 package graph
 
+import "github.com/marciafc/graphql-go/internal/database"
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
